daa6018596090e37882f97fea65ad5410caaf99a
Richard Kong <richardkong9@gmail.com>
2020-12-26 22:26:01 -0500

    No more send

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var port = process.env.PORT || 3001;
 
 app.all('*', function (req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
-    res.send("Hi, welcome to snow quotes api, a compilation of quotes that I personally find interesting. Please see ____.___ to understand how to access this api.");
 
     next();
 });
