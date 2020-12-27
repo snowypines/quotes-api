fac56b47587a4e0d9cf24c2da578a7403eb1d0b6
Richard Kong <richardkong9@gmail.com>
2020-12-26 21:36:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const quoteRepo = require('./quoterepo');
+var app = express();
+var port = process.env.PORT || 3001;
+
+app.all('*', function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('/v1/quotes/:num?', function (req, res) {
+    res.send(quoteRepo.getRandom(req.params.num || 1));
+});
+
+app.listen(port, function () {
+    console.log(`Server running on port http://localhost:${port}`)
+})
---
+++ quotes.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = [
+    {
+        "quote": "The greats never sacrifice the important for the urgent. They handle the immediate problem and still make sure to secure the future.",
+        "author": "Bobby Axelrod", 
+        "source": "Billions - TV Show"
+    },
+    {
+        "quote": "This company will not wait for an uncertain future. We will make our own future.",
+        "author": "Bobby Axelrod", 
+        "source": "Billions - TV Show"
+    },
+    {
+        "quote": "If you're gonna do a thing, do it all the way.",
+        "author": "Bobby Axelrod", 
+        "source": "Billions - TV Show"
+    },
+    {
+        "quote": "Either you confront it head on, or let it go. But don't keep giving power over you by staying in the dark.",
+        "author": "Bobby Axelrod", 
+        "source": "Billions - TV Show"
+    },
+    {
+        "quote": "Hesitation is always easy, rarely useful.",
+        "author": "Quirrell", 
+        "source": "HPMOR - Book"
+    },
+    {
+        "quote": "Do not always pay attention to the words which were spoken but rather pay attention to the mindstate which would have said them.",
+        "author": "Quirrell", 
+        "source": "HPMOR - Book"
+    },
+    {
+        "quote": "Your strength as a rationalist is your ability to be more confused by fiction than by reality.",
+        "author": "Quirrell", 
+        "source": "HPMOR - Book"
+    },
+    {
+        "quote": "Learn how to lose.",
+        "author": "Quirrell", 
+        "source": "HPMOR - Book"
+    },
+    {
+        "quote": "Godric Gryffindor had been a good man, not a happy one.",
+        "author": "A History of Hogwarts", 
+        "source": "HPMOR - Book"
+    },
+    {
+        "quote": "What do the wretched creatures in Azkaban have to offer any faction? Who would benefit from aiding them?",
+        "author": "Quirrell", 
+        "source": "HPMOR - Book"
+    },
+    {
+        "quote": "No rescuer hath the rescuer,\
+                No Lord hath the champion,\
+                No mother and no father,\
+                Nothingness above.",
+        "author": "Godric Gryffindor", 
+        "source": "HPMOR - Book"
+    },
+    {
+        "quote": "The best way to bond with someone isn't doing them a favor. It's asking for one.",
+        "author": "Chuck Rhoades", 
+        "source": "Billions"
+    },
+
+
+];
